44be7a60df76ce68fcdfef9216d4bcdb9e310f53
Sean D'Souza <sdammobubbles@gmail.com>
2019-07-21 16:10:08 -0400

    feat: add footer

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import BadgeList from "./badgelist";
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <footer class="bg-blue-500 p-3 bottom-0 absolute w-full">
+        <BadgeList />
+        <div class="justify-center items-center ">
+          <p class="flex flex-row justify-center text-white">
+            Copyright © Sean D'Souza 2019
+          </p>
+        </div>
+      </footer>
+    );
+  }
+}
