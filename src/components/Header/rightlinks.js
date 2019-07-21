8fa037677e86dbe8ac976ab0b3800865a7cad317
Sean D'Souza <sdammobubbles@gmail.com>
2019-07-21 12:00:40 -0400

    feat: add icons to RightLinks and simplify

--- src/components/Header/rightlinks.js
+++ src/components/Header/rightlinks.js
@@ -1,33 +1,33 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+  faLinkedin,
+  faGithub,
+  faGitlab
+} from "@fortawesome/free-brands-svg-icons";
 
-export default class Logo extends React.Component {
+class RightLink extends React.Component {
   render() {
     return (
-      <div>
-        <a
-          href="/"
-          class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0 mr-4"
-        >
-          Email
-        </a>
-        <a
-          href="/"
-          class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0 mr-4"
-        >
-          LinkedIn
-        </a>
-        <a
-          href="/"
-          class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0 mr-4"
-        >
-          GitHub
-        </a>
-        <a
-          href="/"
-          class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0"
-        >
-          GitLab
-        </a>
+      <a
+        href={this.props.href}
+        class="block text-lg h-10 w-10 flex items-center justify-center leading-none border rounded-full text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white m-2"
+      >
+        <FontAwesomeIcon icon={this.props.icon} />
+      </a>
+    );
+  }
+}
+
+export default class RightLinks extends React.Component {
+  render() {
+    return (
+      <div class="flex flex-row">
+        <RightLink href="/" icon={faEnvelope} />
+        <RightLink href="/" icon={faLinkedin} />
+        <RightLink href="/" icon={faGithub} />
+        <RightLink href="/" icon={faGitlab} />
       </div>
     );
   }
