e385a83142eadb9906080f82bf4568e78fc89c2a
Sean D'Souza <sdammobubbles@gmail.com>
2019-07-22 18:53:14 -0400

    feat: add projects section

--- src/components/Content/content.js
+++ src/components/Content/content.js
@@ -1,11 +1,13 @@
 import React from "react";
 import SectionIntro from "./sectionintro";
+import SectionProjects from "./sectionprojects";
 
 export default class Content extends React.Component {
   render() {
     return (
       <div class="w-full">
         <SectionIntro />
+        <SectionProjects />
       </div>
     );
   }
