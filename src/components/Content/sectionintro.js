bb6de2956b405eb416feaf7d7d91da821f6fbfc0
Sean D'Souza <sdammobubbles@gmail.com>
2019-07-24 17:23:49 -0400

    fix: make header clear and basic parallax with h-screen background

--- src/components/Content/sectionintro.js
+++ src/components/Content/sectionintro.js
@@ -5,10 +5,10 @@ export default class SectionIntro extends React.Component {
   render() {
     return (
       <div
-        class="flex flex-col justify-center items-center bg-scroll bg-center bg-cover"
         style={{ backgroundImage: `url(${Background})` }}
+        class="flex flex-1 block justify-center items-center bg-fixed bg-center bg-cover h-screen"
       >
-        <div class="container mx-auto py-48">
+        <div class="block">
           <h4 class="text-center text-white font-mono">Hey, I'm</h4>
           <h1 class="text-center text-white text-xl font-semibold font-mono">
             Sean D'Souza
