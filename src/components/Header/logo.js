8dfda3530046e7ee2645a05efb562dd44f642d8b
Sean D'Souza <sdammobubbles@gmail.com>
2019-07-25 17:07:55 -0400

    fix: change logo from react to an S for the header and favicon

--- src/components/Header/logo.js
+++ src/components/Header/logo.js
@@ -1,17 +1,17 @@
 import React from "react";
-import logo from "../../logo.svg";
+import logo from "../../assets/logo.svg";
 
 export default class Logo extends React.Component {
   render() {
     return (
       <div class="flex items-center flex-shrink-0 text-white mx-3 opacity-75 hover:opacity-100 justify-center">
-        <a href="/">
+        <a class="font-display headline5" href="/">
           <img
             src={logo}
             alt="logo"
-            width="54"
-            height="54"
-            viewBox="0 0 54 54"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
           />
         </a>
       </div>
