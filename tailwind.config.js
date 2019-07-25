3164b21b31c7e9602e5e12af5e4bdbd30280c528
Sean D'Souza <sdammobubbles@gmail.com>
2019-07-25 16:06:50 -0400

    fix: add fonts and change sans and display, and padding

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {},
+    fontFamily: {
+      sans: [
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Source Sans Pro",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "Noto Sans",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji"
+      ],
+      display: ["Cinzel Decorative", "UnifrakturMaguntia"]
+    }
   },
   variants: {
-    opacity: ['responsive', 'hover', 'focus']
+    opacity: ["responsive", "hover", "focus"]
   },
   plugins: []
-}
+};
