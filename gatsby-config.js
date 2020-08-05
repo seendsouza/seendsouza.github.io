375a5394527bd1f8c91e7f3973df8be06bd6efbc
Sean D'Souza <sdammobubbles@gmail.com>
2020-08-04 20:43:14 -0400

    Add temporary placeholder website

---
+++ gatsby-config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  siteMetadata: {
+    title: `Sean D'Souza`,
+    description: `Sean D'Souza's personal website'`,
+    author: `Sean D'Souza`,
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /src\/images\/.*\.svg/,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#e7e6e3`,
+        theme_color: `#111111`,
+        display: `standalone`,
+        icon: `src/images/seen-logo-512.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-plugin-emotion`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { rhythm } from "../utils/typography";
+import Layout from "../components/layout";
+
+const SocialLinks = ({ data }) => {
+  return data.map((node) => (
+    <div key={node.url} style={{ paddingBottom: "2em" }}>
+      <h3
+        style={{
+          marginBottom: rhythm(1 / 4),
+        }}
+      >
+        {node.title}{" "}
+        <span
+          style={{
+            color: "#bbb",
+          }}
+        >
+          — {node.username}
+        </span>
+      </h3>
+      <a href={node.url} style={{}}>
+        {node.url}
+      </a>
+    </div>
+  ));
+};
+
+export default function Home() {
+  const data = [
+    {
+      icon: "fa-github",
+      title: "GitHub",
+      url: "https://github.com/seendsouza",
+      username: "seendsouza",
+    },
+    {
+      icon: "fa-linkedin",
+      title: "LinkedIn",
+      url: "https://linkedin.com/in/seendsouza",
+      username: "seendsouza",
+    },
+    {
+      icon: "fa-paper",
+      title: "Resume",
+      url:
+        "https://storage.googleapis.com/seendsouza-bucket-0/sean-dsouza-resume.pdf",
+      username: "Sean D'Souza",
+    },
+  ];
+  console.log(data);
+  return (
+    <Layout>
+      <div>
+        <h1
+          style={{
+            display: "inline-block",
+          }}
+        >
+          Site Under Construction
+        </h1>
+        <h4>{data.length} Links</h4>
+        <SocialLinks data={data} />
+        <hr />
+        <p>Site made with Gatsby</p>
+      </div>
+    </Layout>
+  );
+}
