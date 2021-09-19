526a757438b1675c61593a8f5b4a3f29994b8251
seendsouza <seendsouza@users.noreply.github.com>
2021-09-19 15:09:48 +0000

    deploy: 7c24e98a648e59865d5a72f3eb49a6233ac450f8

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4b751e21d33b7de84584.js"
+    "url": "webpack-runtime-2e48a57e5b21a4cc220f.js"
   },
   {
     "url": "framework-b5b229d58c3c710d5429.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3605f2080a29e00a88531c97375d7d9a"
+    "revision": "6f318d24507b6022948ad1243ae04ed5"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
